89fea042a4df72b3738304047f567748eb3e9c6b
Lucas <lbsouza44@gmail.com>
2020-04-15 23:15:44 -0300

    tela inicial e inicio da tela de selecao de perfil

---
+++ DigitalHealth/app/src/main/java/bandtec/com/digitalhealth/SelecionarPerfil.kt
@@ -0,0 +1,12 @@
+package bandtec.com.digitalhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelecionarPerfil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selecionar_perfil)
+    }
+}
