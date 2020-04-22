be2b51150fbed1c590f77b3d6be0da3261ddbf1a
Lucas <lbsouza44@gmail.com>
2020-04-22 19:53:56 -0300

    tela com scroll

--- app/src/main/java/bandtec/com/digitalhealth/MainActivity.kt
+++ app/src/main/java/bandtec/com/digitalhealth/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_paciente)
+        setContentView(R.layout.activity_minhas_receitas_paciente)
     }
 }
